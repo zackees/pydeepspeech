4a027ef2d48a4bd7d14a3a5c664cef796538cbea
zackees <z@zackees.com>
2021-05-19 12:31:08 -0700

    bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ URL = 'https://github.com/zackees/pydeepspeech'
 EMAIL = 'dont@email.me'
 AUTHOR = 'Zach Vorhies'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 
 # The text of the README file
 with open(os.path.join(HERE, "README.md")) as fd:
